137b0bad444449fb1602db9ea7a5f5888b196ae2
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-10-19 23:59:11 +0100

    completed use_effect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,17 +1,30 @@
 // make an api call using useEffect
 import { useEffect, useState } from 'react';
 
+interface APIReponse {
+    completed: boolean,
+    id: number,
+    title: string,
+    userId: 1
+}
+
 export const APICall = () => {
     const [ url, setUrl ] = useState("https://jsonplaceholder.typicode.com/todos/1");
+    const [ results, setResults ] = useState<APIReponse>();
     // Your code here!
 
-    // useEffect(() => {
-    //     const connection = createConnection(serverUrl, roomId);
-    //   connection.connect();
-    //     return () => {
-    //     connection.disconnect();
-    //     };
-    // }, [url]);
+    useEffect(() => {
+        const getResponse = async (): Promise<void> => {
+            const apiResponse = await fetch(url);
+            const responseData = await apiResponse.json();
+            setResults(responseData);
+        }
+        getResponse();
+    }, []);
+
+    useEffect(() => {
+        console.log("new results", results);
+    }, [results]);
 
     return (
         <>
