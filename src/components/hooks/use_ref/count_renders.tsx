c69d9062837c277bcdb9021fb6e0caf411b457c5
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-10-25 15:51:01 +0100

    completed use ref

--- src/components/hooks/use_ref/count_renders.tsx
+++ src/components/hooks/use_ref/count_renders.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
+import { useRef } from 'react';
 
 export const CountRenders = () => {
 	const [value, setValue] = useState('');
-	const count = 1;
+	// const count = 1;
+	const ref = useRef(0);
+
+	function handleClick(value: any){
+		ref.current = ref.current + 1;
+		setValue(value)
+	}
 
 	return (
 		<>
@@ -11,11 +18,34 @@ export const CountRenders = () => {
 			<input
 				value={value}
 				type='text'
-				onChange={(e) => setValue(e.target.value)}
+				onChange={(e) => handleClick(e.target.value)}
 			/>
 
 			<p>{value}</p>
-			<p>I have rendered {count} times</p>
+			<p>I have rendered {ref.current} times</p>
 		</>
 	);
 };
+
+// import { useState } from 'react';
+// import { useRef } from 'react';
+
+// export const CountRenders = () => {
+// 	const [value, setValue] = useState('');
+// 	const count = 1;
+
+// 	return (
+// 		<>
+// 			<h2>useRef</h2>
+
+// 			<input
+// 				value={value}
+// 				type='text'
+// 				onChange={(e) => setValue(e.target.value)}
+// 			/>
+
+// 			<p>{value}</p>
+// 			<p>I have rendered {count} times</p>
+// 		</>
+// 	);
+// };
