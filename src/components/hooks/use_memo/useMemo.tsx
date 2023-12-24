99fe5834cea4fbb9c65f9f5c827ff31cc7d1b482
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-24 01:46:53 +0000

    fixes: add the correct dependency to fix useMemo

--- src/components/hooks/use_memo/useMemo.tsx
+++ src/components/hooks/use_memo/useMemo.tsx
@@ -1,36 +1,40 @@
-import { useState } from 'react';
-import { useMemo } from 'react';
+import { useState, useMemo } from 'react';
 
 export const MemoExample = () => {
+  console.log("Rendering component...");
 
-	console.log("Rendering component...")
+  const [numberObj, setNumberObj] = useState({ input: 0 });
 
-	const [numberObj, setNumberObj] = useState({ input: 0 });
+  const doubleNumber = useMemo(
+    () => slowFunction(numberObj.input),
+	//make sure you reference correcly as numberObj as depedency wont't work for memo because it is creating a new object in setState
+    [numberObj.input]
+  );
 
-	// const doubleNumber = slowFunction(numberObj.input)
+  const handleButtonClick = () => {
+	// this code here commented out will call the slow function each time because the dependency numberObj.input has changed
+    // setNumberObj((prevNumberObj) => ({ input: prevNumberObj.input + 1 }));
+	// This line creates a new object, causing a re-render
+	setNumberObj((prevNumberObj) => ({ input: 0 }));
+  };
 
-	const doubleNumber = useMemo(
-		() => slowFunction(numberObj.input),
-		[numberObj]
-	);
+  return (
+    <>
+      <h2>useMemo</h2>
 
-	return (
-		<>
-			<h2>useMemo</h2>
+      <button onClick={handleButtonClick}>Increment Input</button>
 
-			<button onClick={() => setNumberObj({ input: 3 })}>Double 3</button>
-
-			<p className='use-memo__text'>{doubleNumber}</p>
-		</>
-	);
+      <p className='use-memo__text'>{doubleNumber}</p>
+    </>
+  );
 };
 
 function slowFunction(num: number) {
-	console.log('calling slow function... 🐌');
-	for (let i = 0; i <= 1000000000; i++) {
-		// ⏰
-	}
-	const result = num * 2;
-	console.log(`result: ${result}`);
-	return result;
+  console.log('calling slow function... 🐌');
+  for (let i = 0; i <= 1000000000; i++) {
+    // ⏰ This loop makes the function slow
+  }
+  const result = num * 2;
+  console.log(`result: ${result}`);
+  return result;
 }
